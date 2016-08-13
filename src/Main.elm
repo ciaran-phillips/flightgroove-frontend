bfe6624f35006a781e9150419d48687e8f550c3f
Ciaran Phillips <ciaran@ciaranphillips.com>
2016-08-13 11:30:50 +0000

    Initial commit - set up basic elm project with a static file server (http-server), and add build/serve commands

---
+++ src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App
+
+type alias Model =
+  String
+
+type Msg =
+  NoOp
+
+
+init : ( Model, Cmd Msg)
+init =
+  ("Hello World", Cmd.none)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+view : Model -> Html Msg
+view model =
+  div [] [ text model ]
+
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+  ( model, Cmd.none )
+
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
